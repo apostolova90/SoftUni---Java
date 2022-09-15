d1abb4e9532335de4b171012e4aa13bf08d0a381
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-09-15 13:18:41 +0300

    Create ExcellentResult.java

---
+++ Basics/src/Conditional_Statements/ExcellentResult.java
@@ -0,0 +1,15 @@
+package Conditional_Statements;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number >= 5){
+            System.out.println("Excellent!");
+        }
+    }
+}
