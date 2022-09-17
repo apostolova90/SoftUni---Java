e8bba512bb45882f2bcd81104991eafef6efcc51
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-09-17 09:49:02 +0300

    Create CelsiusToFahrenheit.java

---
+++ Basics/src/MoreExercises/CelsiusToFahrenheit.java
@@ -0,0 +1,13 @@
+package MoreExercises;
+
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double celsius = Double.parseDouble(scanner.nextLine());
+        double fahrenheit  = (double) celsius * 9/5 + 32;
+        System.out.printf("%.2f", fahrenheit);
+    }
+}
