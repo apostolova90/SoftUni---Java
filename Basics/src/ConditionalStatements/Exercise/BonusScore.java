8c174537f70a67851794422e3f42202a627b5905
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-09-20 13:31:58 +0300

    fixing files

--- Basics/src/ConditionalStatements/Exercise/BonusScore.java
+++ Basics/src/ConditionalStatements/Exercise/BonusScore.java
@@ -13,15 +13,15 @@ public class BonusScore {
         if (num <= 100){
             bonusPoints = 5;
         } else if (num > 1000){
-            bonusPoints = bonusPoints * 0.10;
+            bonusPoints = num * 0.10;
         } else {
-            bonusPoints = bonusPoints * 0.20;
+            bonusPoints = num * 0.20;
         }
 
         if (num % 2 == 0){
             bonusPoints = bonusPoints + 1;
         } else if (num % 10 == 5){
-            bonusPoints = bonusPoints + 5;
+            bonusPoints = bonusPoints + 2;
         }
 
         System.out.println(bonusPoints);
