592638e4d2e08e6693428b048d2e5f1f619ac32d
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-10-01 15:01:03 +0300

    Create SumNumbers.java

---
+++ Basics/src/ForLoop/Lab/SumNumbers.java
@@ -0,0 +1,20 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++){ // завъртаме толкова пъти, колкото са ни дадени на първа позиция
+            int currentNum = Integer.parseInt(scanner.nextLine()); // взимам последователно всяко от соатналите във входа числа
+            sum = sum + currentNum;
+        }
+
+        System.out.println(sum);
+    }
+}
