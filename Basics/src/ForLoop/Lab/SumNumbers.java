b057e4c0223ac175b6e2b4ee093ad8a4f8742c2a
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-10-03 09:10:21 +0300

    Update SumNumbers.java

--- Basics/src/ForLoop/Lab/SumNumbers.java
+++ Basics/src/ForLoop/Lab/SumNumbers.java
@@ -6,7 +6,7 @@ public class SumNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.pars                               eInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
 
         int sum = 0;
 
