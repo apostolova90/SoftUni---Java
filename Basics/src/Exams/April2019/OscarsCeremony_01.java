8a9ad21f6f34a2dee6bcc10a24eb615ccf53c1a5
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-11-13 13:43:52 +0200

    Create OscarsCeremony_01.java

---
+++ Basics/src/Exams/April2019/OscarsCeremony_01.java
@@ -0,0 +1,20 @@
+package Exams.April2019;
+
+import java.util.Scanner;
+
+public class OscarsCeremony_01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int priceRoom = Integer.parseInt(scanner.nextLine());
+        double priceStatue = priceRoom * 0.70;
+        double priceCatering = priceStatue * 0.85;
+        double priceMusic = priceCatering * 0.5;
+
+        double totalPrice = priceRoom + priceStatue + priceCatering + priceMusic;
+
+        System.out.printf("%.2f", totalPrice);
+
+    }
+}
