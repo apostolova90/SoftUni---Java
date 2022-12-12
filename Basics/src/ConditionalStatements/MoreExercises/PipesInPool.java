bd0b538b7433dbfee457cfeb581a6749ced07ddf
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-12-12 07:39:45 +0200

    Update PipesInPool.java

--- Basics/src/ConditionalStatements/MoreExercises/PipesInPool.java
+++ Basics/src/ConditionalStatements/MoreExercises/PipesInPool.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class PipesInPool {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double volume = Double.parseDouble(scanner.nextLine());
+        double pipeOne = Double.parseDouble(scanner.nextLine());
+        double pipeTwo = Double.parseDouble(scanner.nextLine());
+        double hours = Double.parseDouble(scanner.nextLine());
+        double workDone = (pipeOne+pipeTwo)*hours;
+        if (volume<workDone){
+            System.out.printf("For %.2f hours the pool overflows with %.2f liters.", hours,Math.abs(volume-workDone));
+        }else{
+            pipeOne = (pipeOne*hours)/workDone*100;
+            pipeTwo = (pipeTwo*hours)/workDone*100;
+            volume = workDone/volume*100;
 
-
+            System.out.printf("The pool is %.2f%%full. Pipe 1: %.2f%%. Pipe 2: %.2f%%.",volume,pipeOne,pipeTwo);
+        }
     }
 }
