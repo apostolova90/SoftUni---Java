770bbf49fa3502d809f33a7feec8b4ebf3e81866
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-12-17 07:30:53 +0200

    Create NumbersFromOneToten_09.java

---
+++ Basics/src/ConditionalStatementsAdvanced/MoreExercises/NumbersFromOneToten_09.java
@@ -0,0 +1,14 @@
+package JavaProgramingBasics.ConditionalStatementsAdvanced.MoreExercises;
+
+import java.util.Scanner;
+
+public class NumbersFromOneToten_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        while (i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
