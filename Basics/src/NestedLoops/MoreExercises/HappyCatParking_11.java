e656bfe04b1178bee540e847047b287ca813c88d
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-12-24 09:41:56 +0200

    Create HappyCatParking_11.java

---
+++ Basics/src/NestedLoops/MoreExercises/HappyCatParking_11.java
@@ -0,0 +1,29 @@
+package NestedLoops.MoreExercises;
+
+import java.util.Scanner;
+
+public class HappyCatParking_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = Integer.parseInt(scanner.nextLine());
+        int hours = Integer.parseInt(scanner.nextLine());
+
+        double totalSum = 0.0;
+
+        for (int i = 1; i <= days ; i++) {
+            double daySum = 0.0;
+            for (int j = 1; j <=hours ; j++) {
+                if (i % 2 != 0 && j % 2 == 0){
+                    daySum += 1.25;
+                }else if(i%2 == 0 && j % 2 != 0){
+                    daySum += 2.5;
+                }else{
+                    daySum++;
+                }
+            }
+            totalSum += daySum;
+            System.out.printf("Day: %d - %.2f leva%n",i,daySum);
+        }
+        System.out.printf("Total: %.2f leva",totalSum);
+    }
+}
