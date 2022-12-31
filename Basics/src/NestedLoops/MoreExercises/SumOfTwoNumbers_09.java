64a0c72612613c59990522d330ef99ed20e10e06
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2022-12-31 07:50:19 +0200

    Create SumOfTwoNumbers_09.java

---
+++ Basics/src/NestedLoops/MoreExercises/SumOfTwoNumbers_09.java
@@ -0,0 +1,35 @@
+package NestedLoops.MoreExercises;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumbers_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+        int magicNumber = Integer.parseInt(scanner.nextLine());
+
+        boolean isValid = false;
+        int combinationCounter = 0;
+
+        for (int i = number1; i <= number2; i++) {
+            for (int j = number1; j <= number2; j++) {
+                combinationCounter++;
+                int sum = i + j;
+                if (sum == magicNumber) {
+                    System.out.printf("Combination N:%d (%d + %d = %d)", combinationCounter, i, j, sum);
+                    isValid = true;
+
+                    break;
+                }
+            }
+            if (isValid) {
+                break;
+            }
+        }
+        if (!isValid) {
+            System.out.printf(" %d combinations - neither equals %d",
+                    combinationCounter, magicNumber);
+        }
+    }
+}
