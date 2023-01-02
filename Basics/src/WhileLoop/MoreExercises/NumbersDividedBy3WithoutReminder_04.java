345b0f1b134342775bf56053a02f97706a435277
apostolova90 <103497053+apostolova90@users.noreply.github.com>
2023-01-02 11:04:41 +0200

    Create NumbersDividedBy3WithoutReminder_04.java

---
+++ Basics/src/WhileLoop/MoreExercises/NumbersDividedBy3WithoutReminder_04.java
@@ -0,0 +1,16 @@
+package WhileLoop.MoreExercises;
+
+import java.util.Scanner;
+
+public class NumbersDividedBy3WithoutReminder_04 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 3; i <= 99; i += 3) {
+            System.out.println(i);
+
+        }
+    }
+}
